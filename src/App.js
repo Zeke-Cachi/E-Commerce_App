a7333834b2c4328e33b527ca8d3ce5a54f5780f1
Zeke-Cachi <ezequielcachi@gmail.com>
2023-04-02 16:43:26 -0300

    se agrega el archivo Carousel.jsx en carpeta components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Carousel from "./components/Carousel/Carousel";
+
+import product1 from "./assets/img/uno.jpg";
+import product2 from "./assets/img/dos.jpg";
+import product3 from "./assets/img/tres.jpg";
+import product4 from "./assets/img/cuatro.jpg";
+import product5 from "./assets/img/cinco.jpg";
+
+
+
+const slides = [
+  {id: 1, productImage: product1},
+  {id: 2, productImage: product2},
+  {id: 3, productImage: product3},
+  {id: 4, productImage: product4,},
+  {id: 5, productImage: product5,},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Carousel slides={slides} />
+
+  )
 }
 
 export default App;
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,80 @@
+/******   IMPORTS ********/
+
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+/******   ICONOS DE REACT-ICONS ********/
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+
+
+
+
+const Carousel = ({slides}) => {
+    
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  /****    CONTROLES DE LOS BOTONES PARA PASAR DE SLIDE    ****/
+
+    const prevSlide = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
+    };
+  
+    const nextSlide = () => {
+      const isLastSlide = currentIndex === slides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    };
+  
+    const goToSlide = (slideIndex) => {
+      setCurrentIndex(slideIndex);
+    };
+
+    /****    AUTO-SLIDER     ****/
+
+    useEffect(() => {
+      let autoScroll = setInterval( () => {
+        nextSlide();
+      }, 3000);
+      return () => {
+        clearTimeout(autoScroll)
+      }
+    }, [currentIndex])
+    
+    
+  
+    return (
+      <div className='h-[800px] w-{100vw} relative group'>
+        <div
+          className='w-full h-full bg-cover bg-center duration-500'
+          style={{ backgroundImage: `url(${slides[currentIndex].productImage})` }}
+        ></div>
+        {/* FLECHA IZQUIERDA */}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* FLECHA DERECHA*/}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+              <div className='flex top-4 justify-center py-2'>
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className='text-2xl cursor-pointer'
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+export default Carousel
